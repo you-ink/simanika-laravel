4af57ba166050036a58b7870a7b8ce94a4c7a202
you(r)-ink <60732521+you-ink@users.noreply.github.com>
2023-06-20 22:52:01 +0700

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -17,15 +17,15 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="{{url('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{url('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="{{url('assets/css/style.css')}}" rel="stylesheet">
 </head>
 
 <body>
@@ -411,18 +411,48 @@
 
   <!-- Vendor JS Files -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="{{url('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{url('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{url('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{url('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{url('assets/vendor/php-email-form/validate.js')}}"></script>
 
   <!-- Template Main JS File -->
   <script src="{{url('assets/js/homepage-main.js')}}"></script>
   <script src="<?= url('assets/js/jquery-3.6.1.min.js') ?>"></script>
   <script src="<?= url('assets/js/dashboard-main.js') ?>"></script>
     <script>
+        function getArtikel() {
+            callApi("GET", "{{ route('api.artikel.index') }}", null, function(req) {
+                member = '';
+                $.each(req.data, function(index, val) {
+                    member += `<div class="col-lg-4 col-md-6 artikel-item filter-${val.divisi.nama.toLowerCase()}">
+                        <div class="artikel-wrap">
+                        <img src="{{ url('') }}${val.sampul}" class="img-fluid w-100" alt="">
+                        <div class="artikel-info">
+                            <h4>${val.judul}</h4>
+                            <p>${val.divisi.nama}</p>
+                            <div class="artikel-links">
+                            <a href="{{ url('') }}${val.sampul}" data-gallery="artikelGallery" class="artikel-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                            <a href="{{ url('') }}/artikel/${val.id}" title="Selengkapnya"><i class="bx bx-link"></i></a>
+                            </div>
+                        </div>
+                        </div>
+                    </div>`;
+                });
+
+                $(".section-artikel").html(member);
+            })
+
+            $(".section-article").attr('data-style', `
+                height: auto !important;
+                background-color: red;
+            `);
+        }
+
+        getArtikel();
+
         $(document).ready(function() {
 
             function getBPH() {
@@ -452,31 +482,6 @@
             getBPH()
 
         })
-
-        function getArtikel() {
-            callApi("GET", "{{ route('api.artikel.index') }}", null, function(req) {
-                member = '';
-                $.each(req.data, function(index, val) {
-                    member += `<div class="col-lg-4 col-md-6 artikel-item filter-${val.divisi.nama.toLowerCase()}">
-                        <div class="artikel-wrap">
-                        <img src="{{ url('') }}${val.sampul}" class="img-fluid w-100" alt="">
-                        <div class="artikel-info">
-                            <h4>${val.judul}</h4>
-                            <p>${val.divisi.nama}</p>
-                            <div class="artikel-links">
-                            <a href="{{ url('') }}${val.sampul}" data-gallery="artikelGallery" class="artikel-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
-                            <a href="{{ url('') }}/artikel/${val.id}" title="Selengkapnya"><i class="bx bx-link"></i></a>
-                            </div>
-                        </div>
-                        </div>
-                    </div>`;
-                });
-
-                $(".section-artikel").html(member);
-            })
-        }
-
-        getArtikel()
 </script>
 </body>
 
